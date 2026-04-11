5aa87a6ef70b0dbad834ac598d62a56da2088bcb
Yomide <ayomidekofoworola59@gmail.com>
2026-04-11 17:21:41 +0100

    Fix: payout init fix

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { CouponsModule } from 'src/coupons/coupons.module';
+import { PaymentsModule } from 'src/payments/payments.module';
 
 @Module({
-  imports: [CouponsModule],
+  imports: [CouponsModule , PaymentsModule],
   controllers: [AdminController],
   providers: [AdminService], // Add AdminService here
 })
