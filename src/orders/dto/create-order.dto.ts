267df5d74db5d3c0e687557956a87615ddc02812
Ayomide Kofoworola <ayomidekofoworola59@gmail.com>
2026-06-14 05:12:18 +0100

    Fix: fix payment page

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,9 +1,84 @@
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
+import { 
+  IsArray, 
+  IsNotEmpty, 
+  IsNumber, 
+  Min, 
+  ValidateNested,
+  IsUUID,
+  ArrayMinSize,
+  IsString,
+  IsOptional
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+// --- SUB-DTO: INDIVIDUAL CAMPAIGN LOG ---
+export class AppliedCampaignDto {
+  @ApiProperty({ example: 'Summer Sale 2024' })
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
+
+  @ApiProperty({ example: 2500 })
+  @Type(() => Number) 
+  @IsNumber()
+  @Min(0)
+  amount!: number;
+}
+
+// --- SUB-DTO: ORDER ITEM ---
+export class OrderItemDto {
+  @ApiProperty({ example: 'uuid-v4-product-id' })
+  @IsUUID()
+  @IsNotEmpty()
+  productId!: string;
+
+  @ApiProperty({ example: 2 })
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
+  quantity!: number;
+
+  @ApiProperty({ example: 15000 })
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  price!: number; 
+}
+
+export class CreateOrderDto {
+  @ApiProperty({ type: [OrderItemDto] })
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items!: OrderItemDto[];
+
+  @ApiProperty({ type: [AppliedCampaignDto] })
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => AppliedCampaignDto)
+  appliedCampaigns?: AppliedCampaignDto[];
+
+  @ApiProperty()
+  @IsUUID()
+  addressId!: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  paymentMethod!: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  shippingMethod?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsUUID()
+  couponId?: string;
+
+  
+}
