c8d9b42798771253a0594eaad17af9663964dcaa
Ayomide Kofoworola <ayomidekofoworola59@gmail.com>
2026-07-08 18:53:21 +0100

    Fix: fix notify pwa

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,3 +1,4 @@
+// src/notification/notification.module.ts
 import { Module, Global, forwardRef } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
@@ -5,7 +6,11 @@ import { MailModule } from '../mail/mail.module';
 
 @Global()
 @Module({
-  imports: [MailModule, forwardRef(() => MailModule),],
+  imports: [
+    // 🟢 FIXED: Removed the eager 'MailModule' reference. 
+    // Leaving ONLY the forwardRef allows NestJS to defer building this loop until startup is ready.
+    forwardRef(() => MailModule),
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
   exports: [NotificationService],
